f38edd3844acf081d2908619c5b8524704660cec
Rafael-acm1 <rafael-acm1@hotmail.com>
2025-02-19 13:51:21 -0300

    implementação da verificação

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,10 @@ public class Main {
 
                 if (escala.gerarEscala()) {
                     todasAsEscalas.put(arquivo.getName(), escala.getEscala());
+
+                    
+                    boolean correto = verificarEscala(escala.getEscala(), bombeiros);
+                    System.out.println("Verificação: " + (correto ? "Saída correta" : "Saída Errada"));
                 } else {
                     System.out.println("Não foi possível gerar escala para " + arquivo.getName());
                 }
@@ -35,5 +39,41 @@ public class Main {
         } catch (IOException e) {
             System.err.println("Erro ao processar arquivos: " + e.getMessage());
         }
+    }
+
+    
+    private static boolean verificarEscala(Map<String, String[][]> escala, List<Bombeiro> bombeiros) {
+        
+        Map<String, Integer> contagemReal = new HashMap<>();
+
+        
+        for (Bombeiro b : bombeiros) {
+            contagemReal.put(b.nome + "_Incêndio", 0);
+            contagemReal.put(b.nome + "_Socorro", 0);
+            contagemReal.put(b.nome + "_Telefone", 0);
+        }
+
+        
+        for (String tarefa : escala.keySet()) {
+            String[][] matriz = escala.get(tarefa);
+            for (String[] dia : matriz) {
+                for (String nome : dia) {
+                    if (contagemReal.containsKey(nome + "_" + tarefa)) {
+                        contagemReal.put(nome + "_" + tarefa, contagemReal.get(nome + "_" + tarefa) + 1);
+                    }
+                }
+            }
+        }
+
+        
+        for (Bombeiro b : bombeiros) {
+            if (contagemReal.get(b.nome + "_Incêndio") != b.incendios ||
+                contagemReal.get(b.nome + "_Socorro") != b.socorros ||
+                contagemReal.get(b.nome + "_Telefone") != b.telefones) {
+                return false; 
+            }
+        }
+
+        return true; 
     }
 }
